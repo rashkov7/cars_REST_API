579d0b057570d16fb75403796f38060d7c0b6ea6
rashkov7 <grashcov@gmail.com>
2022-05-06 09:21:38 +0300

    Added accounts app based on #1 and user is extended based on #2.

---
+++ cars_REST_API/cars_REST_API/accounts/models.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import User, PermissionsMixin
+from django.db import models
+
+from django.utils import timezone
+
+from cars_REST_API.accounts.managers import CustomManager
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+
+    is_staff = models.BooleanField(
+        "staff status",
+        default=False,
+        help_text="Designates whether the user can log into this admin site.",
+    )
+    is_active = models.BooleanField(
+        "active",
+        default=True,
+        help_text=
+            "Designates whether this user should be treated as active. "
+            "Unselect this instead of deleting accounts."
+    )
+    date_joined = models.DateTimeField("date joined", default=timezone.now)
+
+    objects = CustomManager()
+
+    USERNAME_FIELD = "email"
